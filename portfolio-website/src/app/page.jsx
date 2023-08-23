ef6f998be4e9222a681193076dcfafdfdc7f6995
Natha0b <laura.barona04@gmail.com>
2023-08-22 21:32:29 -0500

    Add information in the portfolio

--- portfolio-website/src/app/page.jsx
+++ portfolio-website/src/app/page.jsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from 'react'
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaMailBulk } from 'react-icons/fa';
 
 
-export default function Home() {
+function Home() {
     return (
         <main>
             <header className="header">
@@ -24,18 +26,26 @@ export default function Home() {
 
             <section className="home">
                 <div className="home-content">
-                    <h1>Hi, I´m <span>Nathaly Ortiz</span></h1>
+                    <h1>Hola! soy <span>Nathaly Ortiz</span></h1>
                     <div className="text-animate">
                         <h3>Desarrolladora de Software</h3>
                     </div>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi ratione inventore ducimus ea laborum, facere similique unde dolor impedit explicabo minima magni, error porro deserunt eligendi.</p>
 
                     <div className="btn-box">
-                        <a href="#" className="btn">Hire Me</a>
+                        <a href="#" className="btn">Mi CV</a>
                         <a href="#" className="btn">Let´s Talk</a>
                     </div>
+                </div>
+
+                <div className='home-sci'>
+                    <a href='#'><FaGithub /></a>
+                    <a href='#'><FaLinkedin /></a>
+                    <a href='#'><FaMailBulk /></a>
                 </div>
             </section>
         </main>
     )
-}
+};
+
+export default Home;
