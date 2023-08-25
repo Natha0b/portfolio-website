f27719830537ecc5ed29ca7c59b8f4e9aa866d8a
Natha0b <laura.barona04@gmail.com>
2023-08-24 19:34:38 -0500

    add new component in the project

--- portfolio-website/src/app/components/About/AboutMe.jsx
+++ portfolio-website/src/app/components/About/AboutMe.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
+import './aboutme.css'
 
 function AboutMe() {
-  return (
-    <section className='about'>
-                <h2 className="heading">Sobre <span>Mi</span></h2>
+    return (
+        <section className='about'>
+            <h2 className="heading">Sobre <span>Mi</span></h2>
 
-                <div className="about-img">
-                    <img src="./profile.jpeg" alt="" />
-                    <span className="circle-spin"></span>
-                </div>
+            <div className="about-img">
+                <img src="https://avatars.githubusercontent.com/u/112909840?v=4" alt="Profile_Photo" />
+                <span className="circle-spin"></span>
+            </div>
 
-                <div className="about-content">
-                    <h3>Software Developer!</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure perferendis veniam optio nemo similique aut quam rerum dignissimos ea itaque!</p>
-                    <div className='btn-box btns'>
-                        <a href="#" className='btn'>Leer Mas</a>
-                    </div>
+            <div className="about-content">
+                <h3>Desarrolladora de Software!</h3>
+                <p>
+                    Desarrollador de Software con habilidades técnicas en el mundo del desarrollo tecnológico.
+                    Conocimiento de lenguajes de programación JavaScript, Python y Git, capacidad para crear sitios web
+                    atractivos y funcionales utilizando tecnologías como HTML, CSS y JavaScript. Al igual que el uso frameworks
+                    como React.js para desarrollar aplicaciones web más complejas y manejo de bases de datos relacionales como MySQL.
+                    Como desarrollador de software, estoy constantemente actualizando mis conocimientos y aprendiendo nuevas
+                    tecnologías. Puedo adaptarme rápidamente a los cambios y utilizar las herramientas más adecuadas para cada
+                    proyecto. Con estas habilidades y conocimientos, puedo contribuir al desarrollo y mejora de plataformas
+                    tecnológicas utilizadas en diferentes empresas, optimizar procesos y mejorar la experiencia del cliente.
+                    Estoy comprometida con los estándares de calidad, la eficiencia y el cumplimiento de objetivos.
+                </p>
+                <div className='btn-box btns'>
+                    <a href="#" className='btn'>Leer Mas</a>
                 </div>
-            </section>
-  );
+            </div>
+        </section>
+    );
 }
 
 export default AboutMe;
--- portfolio-website/src/app/components/Home/Home.jsx
+++ portfolio-website/src/app/components/Home/Home.jsx
@@ -6,46 +6,49 @@ import './home.css';
 
 function Home() {
     return (
-            <main>
-                <header className="header">
-                    <a href="#" className="logo">Nathaly.</a>
+        <main>
+            <header className="header">
+                <a href="#" className="logo">Nathaly.</a>
 
-                    <div className="menu-icon"></div>
+                <div className="menu-icon"></div>
 
-                    <nav className="navbar">
-                        <a href="#" className="active">Inicio</a>
-                        <a href="#">Sobre Mi</a>
-                        <a href="#">Porfolio</a>
-                        <a href="#">Educación</a>
-                        <a href="#">Skills</a>
-                        <a href="#">Contacto</a>
+                <nav className="navbar">
+                    <a href="#" className="active">Inicio</a>
+                    <a href="#">Sobre Mi</a>
+                    <a href="#">Porfolio</a>
+                    <a href="#">Educación</a>
+                    <a href="#">Skills</a>
+                    <a href="#">Contacto</a>
 
-                    </nav>
+                </nav>
 
-                </header>
+            </header>
 
-                <section className="home">
-                    <div className="home-content">
-                        <h1>Hola! Soy <span>Nathaly Ortiz</span></h1>
-                        <div className="text-animate">
-                            <h3>Desarrolladora de Software</h3>
-                        </div>
-                        <p>Soy una apasionada del desarrollo frontend, me entusiasma la creacion de interfaces web cautivadoras y funcionales. Mi objetivo es seguir explorando, mejorando e innovando la web en cada proyecto.</p>
-
-                        <div className="btn-box">
-                            <a href="./CV.pdf" className="btn" download>Descarga mi CV</a>
-                        </div>
+            <section className="home">
+                <div className="home-content">
+                    <h1>Hola! Soy <span>Nathaly Ortiz</span></h1>
+                    <div className="text-animate">
+                        <h3>Desarrolladora de Software</h3>
                     </div>
+                    <p>Soy una apasionada del desarrollo frontend, me entusiasma la creacion de interfaces
+                        web cautivadoras y funcionales. Mi objetivo es seguir explorando, mejorando e innovando
+                        la web en cada proyecto.
+                    </p>
 
-                    <div className="home-sci">
-                        <a href='https://github.com/Natha0b'><FaGithub /></a>
-                        <a href='https://www.linkedin.com/in/nathalyortiz/'><FaLinkedin /></a>
-                        <a href="#"><FaMailBulk /></a>
+                    <div className="btn-box">
+                        <a href="./CV.pdf" className="btn" download>Descarga mi CV</a>
                     </div>
+                </div>
 
-                    <div className="home-imgHover"></div>
-                </section>
-            
+                <div className="home-sci">
+                    <a href='https://github.com/Natha0b'><FaGithub /></a>
+                    <a href='https://www.linkedin.com/in/nathalyortiz/'><FaLinkedin /></a>
+                    <a href="#"><FaMailBulk /></a>
+                </div>
+
+                <div className="home-imgHover"></div>
+            </section>
+
         </main >
     )
 };
